56e1ec2e5cb76db4b1c72d559fe3b4a7fced2b89
Meminn <meminbayrak55@gmail.com>
2025-02-18 15:28:28 +0300

    added movie page

---
+++ src/app/movies/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MoviesPage = () => {
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {/* Add your movie-related content here */}
+    </div>
+  );
+};
+
+export default MoviesPage;
