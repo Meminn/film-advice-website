d0f72cba7b4d501cf28c46c64686e27e14ab0d5f
Meminn <meminbayrak55@gmail.com>
2025-03-03 13:10:15 +0300

    refactor: integrate analytics in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from '@vercel/analytics/next';
 import "./globals.css";
 
 const inter = Inter({ 
@@ -67,6 +68,7 @@ export default function RootLayout({
 
             <main className="flex-1">
               {children}
+              <Analytics/>
             </main>
 
             <footer className="border-t">
